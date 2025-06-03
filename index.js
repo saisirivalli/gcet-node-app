c8696bd8a209ac4a6e39a5182fc0840987de5bb5
saisirivalli <23r11a0512@gcet.edu.in>
2025-06-03 10:06:16 +0530

    updated

---
+++ index.js
@@ -0,0 +1,29 @@
+import cors from "cors";
+const app = express();
+app.listen(8080, () => {
+  console.log("Server Started");
+});
+
+app.use(cors());
+app.get("/", (req, res) => {
+  return res.send("Good Morning");
+});
+app.get("/greet", (req, res) => {
+  res.send("Greetings");
+});
+app.get("/name", (req, res) => {
+  res.send("Siri");
+});
+
+app.get("/weather", (req, res) => {
+  res.send("31degree");
+});
+
+app.get("/products", (req, res) => {
+  const products = [
+    { name: "Product 1", price: 34 },
+    { name: "Product 2", price: 64 },
+    { name: "Product 3", price: 45 },
+  ];
+  res.json(products);
+});
