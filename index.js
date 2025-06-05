5f553e7b591d066f2677e2858ba813e6bddff545
saisirivalli <23r11a0512@gcet.edu.in>
2025-06-05 09:45:36 +0530

    updated

--- index.js
+++ index.js
@@ -1,63 +1,34 @@
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from 'dotenv';
+import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+
+dotenv.config();
+
 const app = express();
-app.listen(8080, () => {
-  console.log("Server Started");
-});
 app.use(cors());
-app.get("/", (req, res) => {
-  return res.send("Good Morning");
-});
+app.use(express.json());
 
-app.get("/greet", (req, res) => {
-  res.send("Greetings");
-});
+const DBUSER = encodeURIComponent(process.env.DBUSER)
+const DBPASS = encodeURIComponent(process.env.DBPASS)
+const MONGO_URI =`mongodb+srv://${DBUSER}:${DBPASS}@cluster0.qjxhv.mongodb.net/gcet?retryWrites=true&w=majority&appName=Cluste`
 
-app.get("/name", (req, res) => {
-  res.send("siri");
-});
+// const MONGO_URI = process.env.MONGO_URI
+//testing
+app.use("/users", userRouter);
+app.use("/products", productRouter);
+app.use("/orders",orderRouter)
 
-app.get("/weather", (req, res) => {
-  res.send("31degree");
-});
-
-app.get("/products", (req, res) => {
-  const products = [
-    { name: "Product 1", price: 34 },
-    { name: "Product 2", price: 64 },
-    { name: "Product 3", price: 45 },
-  ];
-  res.json(products);
-});
-
-// import express from "express";
-// import cors from "cors";
-// const app = express();
-// app.listen(8080, () => {
-//   console.log("Server Started on port 8080");
-// });
-
-// app.use(express.json());
-// app.use(cors());
-// const users = [];
-
-// app.get("/", (req, res) => {
-//   return res.json(users);
-// });
-
-// app.post("/register", (req, res) => {
-//   const { name, email, pass } = req.body;
-//   users.push({ name, email, pass });
-//   return res.json(users);
-// });
-
-// app.post("/login", (req, res) => {
-//   const { email, pass } = req.body;
-//   const found = users.find(
-//     (value) => value.email === email && value.pass === pass
-//   );
-//   if (!found) {
-//     res.json({message:"User not found"})
-//   }
-//   res.json({...found,token:"123"});
-// });
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server Started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
