d4cdde8921315a3e6bdf5505abaf09a65350e908
saisirivalli <23r11a0512@gcet.edu.in>
2025-07-17 19:18:30 +0530

    updated

--- models/productModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-const productSchema = mongoose.Schema({
-  name: { type: String },
-  price: { type: Number },
-});
-// const product = mongoose.model("Product", productSchema);
-
-export default mongoose.model("Product", productSchema);
